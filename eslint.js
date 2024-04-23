1119dd6e7bf5fb6f8bbee5dc3967add128b5db91
MuhammedEman14 <129196046+MuhammedEman14@users.noreply.github.com>
2024-04-23 23:07:29 +0500

    Add files via upload

---
+++ eslint.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+      "browser": true,
+      "es2021": true,
+      "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+      "ecmaVersion": 12,
+      "sourceType": "module"
+    },
+    "rules": {
+      "indent": ["error", 2], // enforce 2 space indentation
+      "quotes": ["error", "single"], // enforce single quotes
+      "semi": ["error", "always"], // enforce semicolons at the end of statements
+      "no-unused-vars": "warn", // warn about unused variables
+      // Add more custom rules here
+    }
+  };
+  
